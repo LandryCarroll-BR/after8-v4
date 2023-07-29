66d675ba8f6af83ab4f349be3107e3ab745faf20
Landry Carroll <landry@boldist.co>
2023-07-29 11:16:41 -0400

    refactor: better header styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,11 @@
 import { cn } from '@/lib/utils';
 import { gql } from '../../__generated__';
-import { motion } from 'framer-motion';
-import { useScrollPosition } from '@/hooks/use-scroll';
+import { motion, useMotionTemplate, useTransform } from 'framer-motion';
 import { MenuIcon } from '../icons';
 import { PrimaryMenuItemFragmentFragment } from '../../__generated__/graphql';
 
 import {
+  Container,
   Logo,
   NavigationMenu,
   Sheet,
@@ -15,57 +15,68 @@ import {
 } from '@/components';
 
 import Link from 'next/link';
+import { useBoundedScroll } from '@/hooks/use-bounded-scroll';
 
 interface HeaderProps extends React.HTMLProps<HTMLElement> {
   menuItems: PrimaryMenuItemFragmentFragment[];
 }
 
-export const Header = ({ className, menuItems }: HeaderProps) => {
-  let { height: headerHeight } = useScrollPosition(50, 80);
-  let { height: logoHeight } = useScrollPosition(40, 60);
+export const Header = ({ menuItems }: HeaderProps) => {
+  let { scrollYBoundedProgress } = useBoundedScroll(100);
+  let scrollYBoundedProgressThrottled = useTransform(
+    scrollYBoundedProgress,
+    [0, 0.75, 1],
+    [0, 0, 1]
+  );
 
   return (
     <motion.header
-      style={{ height: headerHeight }}
-      className={cn(
-        'fixed top-0 z-40 flex w-full items-center border-border bg-background/80 py-2 pt-8',
-        className
-      )}
+      style={{
+        height: useTransform(scrollYBoundedProgressThrottled, [0, 1], [100, 70]),
+        backgroundColor: useMotionTemplate`rgb(14 14 17 / ${useTransform(
+          scrollYBoundedProgressThrottled,
+          [0, 1],
+          [1, 0.1]
+        )})`,
+      }}
+      className="fixed inset-x-0 flex h-20 shadow backdrop-blur-md"
     >
-      <div className="container">
-        <div className={'flex w-full items-center gap-5'}>
-          <motion.div style={{ height: logoHeight }} className={cn('')}>
-            <Link href="/" title="Home" className="">
-              <Logo className="aspect-auto h-full w-full" />
-            </Link>
-          </motion.div>
+      <Container className="flex w-full items-center gap-8 pt-4">
+        <motion.div
+          style={{ height: useTransform(scrollYBoundedProgressThrottled, [0, 1], [80, 54]) }}
+        >
+          <Logo className="aspect-auto h-full w-full" />
+        </motion.div>
+        <motion.div
+          className="hidden lg:block"
+          style={{ opacity: useTransform(scrollYBoundedProgressThrottled, [0, 1], [1, 0]) }}
+        >
+          <NavigationMenu menuItems={menuItems} />
+        </motion.div>
 
-          <NavigationMenu className={'hidden lg:flex'} menuItems={menuItems}></NavigationMenu>
-
-          <div className="ml-auto block lg:hidden">
-            <Sheet>
-              <SheetTrigger
-                className={cn(
-                  buttonVariants({
-                    variant: 'outline',
-                    size: 'lg',
-                    className: 'flex items-center justify-center',
-                  })
-                )}
-              >
-                <MenuIcon className="h-8 w-8 text-primary" />
-              </SheetTrigger>
-              <SheetContent className="border-0 border-l pt-16">
-                <NavigationMenu
-                  id={''}
-                  className={'flex-col gap-4 text-right !text-2xl !text-primary'}
-                  menuItems={menuItems}
-                ></NavigationMenu>
-              </SheetContent>
-            </Sheet>
-          </div>
+        <div className="ml-auto block lg:hidden">
+          <Sheet>
+            <SheetTrigger
+              className={cn(
+                buttonVariants({
+                  variant: 'outline',
+                  size: 'lg',
+                  className: 'flex items-center justify-center',
+                })
+              )}
+            >
+              <MenuIcon className="h-8 w-8 text-primary" />
+            </SheetTrigger>
+            <SheetContent className="border-0 border-l pt-16">
+              <NavigationMenu
+                id={''}
+                className={'flex-col gap-4 text-right !text-2xl !text-primary'}
+                menuItems={menuItems}
+              ></NavigationMenu>
+            </SheetContent>
+          </Sheet>
         </div>
-      </div>
+      </Container>
     </motion.header>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import { Blink } from './blink/Blink';
 import { Button, buttonVariants } from './button/Button';
+import { Container } from './container/container';
 import { Header } from './header/header';
 import { Logo } from './logo/logo';
 import { NavigationMenu } from './navigation-menu/navigation-menu';
@@ -23,6 +24,7 @@ export {
   Blink,
   Button,
   buttonVariants,
+  Container,
   Footer,
   Header,
   Logo,
